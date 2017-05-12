82df49ece12d4e73d1d3778fa7c9743940804f22
Lionel Aaron Dsouza <laren2j@gmail.com>
2017-05-12 01:05:25 -0400

    changes to protfolio and code cleanup

--- includes/js/init.js
+++ includes/js/init.js
@@ -66,65 +66,12 @@ $(document).ready(function() {
     var wow = new WOW({ mobile: false });
     wow.init();
 
-    /***************************************************************************
-          CONTACT FORM
-    ***************************************************************************/
-
-    $("#contactForm").validator().on("submit", function (event) {
-        if (event.isDefaultPrevented()) {
-            // handle the invalid form...
-            formError();
-            submitMSG(false, "Did you fill in the form properly?");
-        } else {
-            // everything looks good!
-            event.preventDefault();
-            submitForm();
-        }
-    });
-    function submitForm(){
-    // Initiate Variables With Form Content
-        var name = $("#name").val();
-        var email = $("#email").val();
-        var message = $("#message").val();
-        
-        $.ajax({
-            type: "POST",
-            url: "process.php",
-            data: "name=" + name + "&email=" + email + "&message=" + message,
-            success : function(text){
-                if (text == "success"){
-                    formSuccess();
-                } else {
-                    formError();
-                    submitMSG(false,text);
-                }
-            }
-        });
-    }
-    function formSuccess(){
-        $("#contactForm")[0].reset();
-        submitMSG(true, "Message Sent!")
-    }
-    function formError(){
-        $("#contactForm").removeClass().addClass('shake animated').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', 
-        function(){
-            $(this).removeClass();
-        });
-    }
-    function submitMSG(valid, msg){
-        if(valid){
-            var msgClasses = "h3 text-center fadeInUp animated text-success";
-        } else {
-            var msgClasses = "h3 text-center text-danger";
-        }
-        $("#msgSubmit").removeClass().addClass(msgClasses).text(msg);
-    }
     
     
     /**************************************************************************
        Projects
     **************************************************************************/
-    $('#portfolio-item').mixItUp();
+    // $('#portfolio-item').mixItUp();
     
     $('.sa-view-project-detail').on('click', function(event) {
         event.preventDefault();
